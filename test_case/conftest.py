1af00c66ab145d421ab89f8a96f76048657dc399
Jian Sun <sunusd@yahoo.com>
2021-06-16 11:49:43 -0700

    Update conftest.py

--- test_case/conftest.py
+++ test_case/conftest.py
@@ -17,6 +17,7 @@ def env_config(request):
     project_name = 'api_auto_test'
     rootPath = get_root_path(project_name)
     config_path = os.path.abspath(rootPath + 'config\\env_config.yml')  # 获取tran.csv文件的路径
+    test_id = 10
     with open(config_path) as f:
         env_config = yaml.load(f) #读取配置文件
     return env_config
